5632a27fbd6a05c9ed93e52928ad51a04bfcfbec
EvansRobbie <evansitoden94@gmail.com>
2023-06-07 02:09:33 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-red-500'>
+        Hello
+    </div>
+  )
+}
+
+export default App
