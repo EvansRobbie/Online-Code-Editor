0530edab46c54ba7e6e9a962c748765e14f120b2
EvansRobbie <evansitoden94@gmail.com>
2023-06-07 06:14:59 +0300

    add collapse and expand functionality

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'codemirror/mode/xml/xml';
@@ -6,22 +7,34 @@ import 'codemirror/mode/javascript/javascript';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 
 const Editor = ({displayName, languageName, value, onChange}:{displayName:string, languageName:string, onChange:React.SetStateAction<React.Dispatch<string>>, value:string}) => {
-    
+    const [isOpen, setIsopen] = useState(true)
     const handleChange = (editor:any, data:any, value:any) =>{
         onChange(value)
     } 
+    // console.log(isOpen)
   return (
-   <div className="w-full border border-gray-500   ">
+   <div className={` ${ isOpen ? ' flex-grow ' : 'flex-grow-0 flex-shrink  '} relative  border border-gray-500`}>
     <div className="flex w-full justify-between  items-center">
         <div className='h-10 text-slate-400 font-bold text-center pt-2 bg-[#263238] w-20'>
             {displayName}
         </div>
-     <button>Open</button>
+        <div>
+
+     <button onClick={() => setIsopen(!isOpen)} className='text-slate-200 cursor-pointer'>
+        {isOpen ?  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25" />
+</svg>
+:    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+</svg>
+}
+     </button>
+        </div>
     </div>
     <CodeMirror
         onBeforeChange={handleChange}
         value={value}
-        className=''
+        className='codeMirror'
         options={{
             lineWrapping:true,
             lint:true,
--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Output from './Output'
 import Editor from './Editor'
 
@@ -6,11 +6,25 @@ const Index = () => {
     const [html, setHtml] = useState<string>('')
     const [css, setCss] = useState<string>('')
     const [javascript, setJavascript] = useState<string>('')
+    const [srcDoc, setSrcDoc] = useState('')
+ useEffect (() =>{
+    const timeOut = setTimeout(() =>{
+        setSrcDoc(`
+        <html>
+            <body>${html}</body>
+            <style>${css}</style>
+            <script>${javascript}</script>
+        </html>
+        `)
+    }, 250)
+    return () => clearTimeout(timeOut)
+ }, [html, css, javascript])
+    
   return (
     <div className='h-screen'>
         {/* Editor */}
-        <div className='flex bg-black h-[60vh] py-4'>
-        <div className='border flex w-full gap-4 border-gray-500'>
+        <div className='flex  bg-black h-[60vh] py-4'>
+        <div className='border flex flex-grow gap-4 border-gray-500'>
             <Editor displayName='HTML' languageName='xml' onChange={setHtml}  value={html}/>
             <Editor displayName='CSS' languageName='CSS' onChange={setCss}  value={css}/>
             <Editor displayName='JS' languageName='JS' onChange={setJavascript}  value={javascript}/>
@@ -19,7 +33,7 @@ const Index = () => {
     </div>
         {/* Output display */}
         <div className=''>
-        <iframe src="" title='output'
+        <iframe srcDoc={srcDoc} title='output'
                 sandbox='allow-scripts'
                 height={100}
                 width={100}
